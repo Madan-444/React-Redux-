1642b09c3c6f5f9da501f1ba01e84b8c3940c1ba
Madan-444 <mmadan3600@gmail.com>
2021-02-20 13:46:03 +0545

    what is

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,19 +1,20 @@
 const redux = require("redux")
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM ='BUY_ICECREAM'
 
 function buyCake() {
     return {
-        type: BUY_CAKE,
-        info: 'First redux action'
+        type: BUY_CAKE
+        // info: 'First redux action'
     }
 }
 function buyIcream() {
     return {
-        type: BUY_ICECREAM,
-        info: 'Second redux action'
+        type: BUY_ICECREAM
+        // info: 'Second redux action'
     }
 }
 
