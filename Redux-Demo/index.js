2869ae18e1e61250a40e15da97543e33973b41d8
Madan-444 <mmadan3600@gmail.com>
2021-02-20 14:54:36 +0545

    forth commit

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,6 +1,10 @@
 const redux = require("redux")
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const reduxLogger = require('redux-logger')
+
+const applyMiddleware  = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM ='BUY_ICECREAM'
@@ -51,10 +55,10 @@ const rootReducer = combineReducers({
     a: cakeReducer,
     b: iceCreamReducer
 })
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(logger))
 
 console.log('Initial state',store.getState())
-const unsubscribe = store.subscribe(()=> console.log('Updated State',store.getState()))
+const unsubscribe = store.subscribe(()=> {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
